e956e89f5dcba90cfbb65bfdee89a0f5dec2ce85
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-11-08 20:09:20 +0000

    hw has been completed

--- .vscode/2-letConstVar/odev.js
+++ .vscode/2-letConstVar/odev.js
@@ -5,8 +5,16 @@
 4.Show the value of admin using alert (must output “John”).
 */
 
+let admin, name;
+
+name = "John";
+admin = name;
+// alert(admin);
+
 /**2
  * 
  * 1. Create a variable with the name of our planet. How would you name such a variable?
    2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
  */
+const ourPlanet = "Earth";
+let visitor;
